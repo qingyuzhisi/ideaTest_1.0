5a0abd04e7d3f75accd344af549691e0cda6b6fe
xulong <long.xu@ele.me>
2017-07-03 16:24:06 +0800

    2st sample on dev

--- src/main/scala/com/eleme/sr/MyRouteBuilder.scala
+++ src/main/scala/com/eleme/sr/MyRouteBuilder.scala
@@ -23,4 +23,7 @@ class MyRouteBuilder(override val context : CamelContext) extends ScalaRouteBuil
     println("1st sample on dev")
   }
 
+  def test2():Unit={
+    println("2st sample on dev")
+  }
 }
